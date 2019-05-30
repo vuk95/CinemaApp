ad3de805bc4c90c553b9802a286c00d8f0fffc0b
vuk95 <vuk.novakovic@pbssolutions.net>
2019-05-30 16:21:10 +0200

    Initial setup

---
+++ CinemaBE/src/main/java/com/cinemapp/CinemaBE/CinemaBeApplication.java
@@ -0,0 +1,13 @@
+package com.cinemapp.CinemaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBeApplication.class, args);
+	}
+
+}
